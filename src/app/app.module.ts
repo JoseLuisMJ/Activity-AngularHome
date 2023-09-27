98b33cbd8d57eed4a59564d2cb3d6b8262979334
JoseLuisMJ <trepa01@hotmail.com>
2023-09-28 00:01:51 +0200

    add folder and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    HomeComponent,
+    HousingLocationComponent
   ],
   imports: [
     BrowserModule,
